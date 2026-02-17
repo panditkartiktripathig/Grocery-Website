63997d71662acb5029311a84a40e8291a33b0443
panditkartiktripathig <panditkartiktripathig@gmail.com>
2026-02-17 21:52:46 +0530

    lecture-10

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ document.querySelector('#search-btn').onclick = () =>
 {
     searchForm.classList.toggle('active');
 }
+
+
+
+
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>
+{
+    shoppingCart.classList.toggle('active');
+}
+
+
+
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>
+{
+    loginForm.classList.toggle('active');
+}
